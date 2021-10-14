4f5f417a658b8a6550cae314b6fd6d9b201b297d
TiangeWei <twei14@ucsc.edu>
2021-10-14 19:28:50 +0800

    fixed some error

---
+++ lab4/js/lab.js
@@ -0,0 +1,26 @@
+// Summary. Art101 Lab4: Pseudocodding and Problem-sovling
+// Description. Get in the habit of thinking in terms of pseudocoding before jumping into coding.
+// @link   URL
+// @file   This files is for lab4. It contains Pseudocodes of tasks.
+// @author Tiange Wei
+// @since  10-14-2021
+
+
+// Pseudocode of Everyday Task:
+//   How to Order a Ubereat:
+//     1.Decide what you want to eat.
+//     2.Find that restaurant on Ubereat.
+//     3.Add all the food you want to the shopping cart.
+//     4.Pay for the food.
+//
+// Pseudocode of Computer Task:
+//   How to make a 3D model for a game:
+//     1.Find concept art images that shows the appearence of the object.
+//     2.Open Maya (or any other 3D modeling software).
+//       3.Make high resolution mesh, include all the details that show on the concept art.
+//       4.Make low resolution mesh that matches the silhouette of the high resolution mesh.
+//       5.Edit the UV map of the low-res mesh.
+//     6.Export the low-res mesh from Maya to Substance Painter to bake normal map and AO map.
+//       7.Create textures for the mesh.
+//     8.Export the low-res mesh and its textures to a game engine like UE.
+//       9.Put the mesh into the scene and apply material for it using the textures.
