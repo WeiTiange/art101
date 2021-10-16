3f2c38b480014c8b78a085e21bf04c0f58910880
TiangeWei <twei14@ucsc.edu>
2021-10-16 15:04:11 +0800

    lab5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+//
+// Author: Tiange Wei <twei14@ucsc.edu>
+// Created: 16 October, 2021
+// License: Public Domain
+//
+
+//Define Variables
+var make = "Ford";
+var model = "Taurus";
+var color = "Black";
+var year = "1988";
+
+//Calculate
+var age = 2021 - year;
+
+//Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " year<br>");
